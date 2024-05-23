a76b85b6dee60841a16ce97388a9faf65aea749f
Maren Varus <marenvar@re-n2.skd.21-school.uz>
2024-05-23 18:13:01 +0500

    Some Learn

---
+++ src/TypeFunctions/typdefPointers1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// определяется тип message
+typedef void(message)(void);
+
+void hello() {printf("Hello, World \n");}
+void goodbye() {printf("Good Bye, World \n");}
+
+int main(){
+    message *mes1 =  &hello; // указатель указывает на функцию hello
+    message *mes2 = &goodbye; // указатель указывает на функцию goodbye
+
+    mes1();
+    mes2();
+
+    return 0;
+}
